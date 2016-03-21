50da753e35d92a6c9b7ba69b3c189d0806f55872
hsharm3 <h.sharma09@gmail.com>
2016-03-20 22:44:44 -0500

    Committing final version of the matrix investion code.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inm <- NULL
+  set <- function(y) {
+    x <<- y
+    inm <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) inm <<- solve
+  getsolve <- function() inm
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -12,4 +22,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inm <- x$getsolve()
+  if(!is.null(inm)) {
+    message("getting cached data")
+    return(inm)
+  }
+  data <- x$get()
+  inm <- solve(data, ...)
+  x$setsolve(inm)
+  inm
 }
+
+#Input
+him2 <- matrix(1:4,2,2)
+
+#Trial Run
+cacheSolve(makeCacheMatrix(him2))
